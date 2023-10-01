37db480e5cd7a909e2985844fbf4fec568ddbdd6
evgg0r <gor.evg2100@gmail.com>
2023-10-01 21:20:56 +0700

    commit

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-function xxx() {
-    console.log('blabla');
+// function getSum(a, b, c) {
+//     const sum = a + b + c;
+//     return sum;
+// }
+
+// function getGreetingMassege(userName, companyName) {
+//     return "Hello , " + name + "! Welcome to " + name2;
+// }
+
+// const result = getSum(10, 20, 30);
+// // console.log(result);
+
+// const massege = getGreetingMassege('Shenia', 'IT-INCUBATOR.IO');
+// console.log(massege);
+
+function bla() {
+const sum = 10;
+console.log(sum);
 }
 
-xxx();
+const sum = 100;
+console.log(sum);
 
-// console.log('it-kamasutra');
-// document.write('it-itcubator.io');
-
-// console.log(1);
-// document.write(1);
-
-// console.log('hello from batumi');
-// document.write('it-itcubator.io');
+bla();
