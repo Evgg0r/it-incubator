1e49b6b35210f77d99b1c232eed0e8bbf90f9635
evgg0r <gor.evg2100@gmail.com>
2023-10-07 11:21:18 +0700

    commit

--- 05/index.js
+++ 05/index.js
@@ -1,10 +1,10 @@
-let location1 = 3; 
-let location2 = 4;
-let location3 = 5;
-let guess; 
-let hits = 0; 
-let guesses = 0; 
-let isSunk = false; 
+let location1 = Math.floor(Math.random() * 5)
+let location2 = location1 + 1;
+let location3 = location2 + 2;
+let guess;
+let hits = 0;
+let guesses = 0;
+let isSunk = false;
 
 while (isSunk == false) {
     guess = prompt("Ready, aim, fire! (enter a number 0-6):");
@@ -31,4 +31,3 @@ while (isSunk == false) {
 let stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3 / guesses);
 
 alert(stats);
-
