98763ab5c0360eb38a54c9ec092d9952d4170006
evgg0r <gor.evg2100@gmail.com>
2023-10-07 10:55:45 +0700

    commit

--- 05/index.js
+++ 05/index.js
@@ -1,5 +1,34 @@
-let books = ["Война и мир", "Мастер и маргарита", "Преступление и наказание"]
+let location1 = 3; 
+let location2 = 4;
+let location3 = 5;
+let guess; 
+let hits = 0; 
+let guesses = 0; 
+let isSunk = false; 
 
-for (let i = 0; i < books.length; i++) {
-    document.write(books[i], '<br>');
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number 0-6):");
+
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
+            hits = hits + 1;
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        } else {
+            alert("MISS");
+        }
+    }
 }
+
+let stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + (3 / guesses);
+
+alert(stats);
+
