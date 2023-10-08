b9fce813cc6c8772910f332864c8619e508ab375
evgg0r <gor.evg2100@gmail.com>
2023-10-08 19:54:46 +0700

    commit

--- 05/index.js
+++ 05/index.js
@@ -81,3 +81,46 @@
 // makePhrases();
 
 // ------------------------------
+// var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+// var hasBubbleGum = [false, false, false, true];
+
+// for (var i = 0; i < hasBubbleGum.length; i++) {
+//     if (hasBubbleGum[i]) {
+//         console.log(products[i] +
+//             " contains bubble gum");
+//     }
+
+// }
+
+// ------------------------------
+
+let scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
+
+function printAndGetHighScore(scores) {
+    let highScore = 0;
+    var output;
+    for (var i = 0; i < scores.length; i++) {
+        output = "Bubble solution #" + i + " score: " + scores[i];
+        console.log(output);
+        if (scores[i] > highScore) {
+            highScore = scores[i];
+        }
+    }
+    return highScore;
+}
+
+function getBestResults(scores, highScore) {
+    var bestSolutions = [];
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            bestSolutions.push(i);
+        }
+    }
+    return bestSolutions;
+}
+let highScore = printAndGetHighScore(scores);
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest bubble score: " + highScore);
+
+let bestSolutions = getBestResults(scores, highScore);
+console.log("Solutions with the highest score: " + bestSolutions);
