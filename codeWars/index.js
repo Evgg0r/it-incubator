7336ab7d3fd5ca399b3150618a8f8ffdeca5a318
evgg0r <gor.evg2100@gmail.com>
2023-11-04 17:14:54 +0700

    commit

--- codeWars/index.js
+++ codeWars/index.js
@@ -4,6 +4,7 @@
 //     Return true
 // if yes, false otherwise: )
 
+
 // function hero(bullets, dragons) {
 //     if (bullets >= dragons * 2) {
 //         return true;
@@ -12,4 +13,19 @@
 //     }
 // }
 
-//-------------------------------
+//---------------------------
+
+// Make a
+// function that will
+// return a greeting statement that uses an input;
+// your program should
+// return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+// function greet(name) {
+//     return "Hello, " + name + " how are you doing today?";
+// }
+
+//---------------------------
