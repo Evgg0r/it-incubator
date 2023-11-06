34b0ab8224e9b1e7af7cacd7f5a8952d92f1b669
evgg0r <gor.evg2100@gmail.com>
2023-11-06 18:06:41 +0700

    commit

--- freeCodeCamp/JS/index.js
+++ freeCodeCamp/JS/index.js
@@ -80,3 +80,22 @@ function checkObj(obj, checkProp) {
 }
 
 //-------------------------------
+
+const myMusic = [{
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+        "CD",
+        "8T",
+        "LP"
+    ],
+    "gold": true
+}, {
+    "artist": "new artist",
+    "title": "new title",
+    "release_year": 2023,
+    "formats": ["CD", "mp3", "vinil"]
+}];
+
+//-------------------------------
