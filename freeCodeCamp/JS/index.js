c3d1c18cea1b0c2076e2d6757b336c8c36eb1e03
evgg0r <gor.evg2100@gmail.com>
2023-11-06 17:46:44 +0700

    commit

--- freeCodeCamp/JS/index.js
+++ freeCodeCamp/JS/index.js
@@ -67,3 +67,16 @@ cc('K');
 cc('A');
 
 //-------------------------------
+
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+
+    if (obj.hasOwnProperty(checkProp)) {
+        return obj[checkProp]
+    } else {
+        return "Not Found";
+    }
+    // Only change code above this line
+}
+
+//-------------------------------
