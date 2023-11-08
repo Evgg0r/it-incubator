f5f468dd373b47bc62d1db3e5b12187306ede6ea
evgg0r <gor.evg2100@gmail.com>
2023-11-08 20:08:47 +0700

    commit

---
+++ Sandbox/index.js
@@ -0,0 +1,7 @@
+let name = 'it-incubator';
+let graduatesCount = 2000;
+let areYouChampion = true;
+
+document.write('<h1>', name, '</h1>');
+document.write('<input type="number" value="' + graduatesCount + ">'");
+document.write('<input type="checkbox"', areYouChampion ? ' checked ': '', '>');
---
+++ Sandbox/index3.js
@@ -0,0 +1,13 @@
+let student = {
+    info: {
+        firsName: 'Ivan',
+        lastName: 'Ivanov',
+    },
+    address: {
+        country: 'France',
+        city: 'Paris',
+    }
+}
+
+document.write('<b>firstName:</b> ', student.info.firstName, '<br>');
+document.write('<b>'lastName:</b> ')
