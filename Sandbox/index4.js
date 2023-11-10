5b145b190775cc955b5babc8a41ed8ca645c4c2c
evgg0r <gor.evg2100@gmail.com>
2023-11-11 04:18:31 +0700

    commit

--- Sandbox/index4.js
+++ Sandbox/index4.js
@@ -12,7 +12,7 @@ let card1 = {
     number: 5282345678901289,
     expirationYear: 2025,
     expirationMonth: 9,
-    isActive: true
+    isActive: true,
     transaction: {
         title: 'Order Revenue',
         amount: 874,
@@ -29,13 +29,12 @@ let card2 = {
     number: 59232654321012032,
     expirationYear: 2026,
     expirationMonth: 4,
-    isActive: false
+    isActive: false,
     transaction: {
         title: 'Order Revenue',
         amount: 874,
         date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
     }
-
 };
 
 let card3 = {
@@ -46,7 +45,7 @@ let card3 = {
     number: 5543259349582930,
     expirationYear: 2028,
     expirationMonth: 3,
-    isActive: true
+    isActive: true,
     transaction: {
         title: 'Order Revenue',
         amount: 874,
@@ -73,16 +72,20 @@ for (let i = 0; i <= cards.length; i++) {
         card1.expirationYear,
         '</span>'
     );
+
+    document.write('<h2>', 'History Transaction', '</h2>');
     document.write("<ul>");
     document.write(
         "<li>",
-        card.transaction.title, 
-        ", ", 
-        card.transaction.date, 
-        ",", 
-        card.transaction.amount, 
+        card.transaction.title,
+        ", ",
+        card.transaction.date,
+        ",",
+        card.transaction.amount,
         "</li>"
-        );
+    );
     document.write("</ul>");
     document.write('</div>');
+
+    document.write('<hr>');
 }
