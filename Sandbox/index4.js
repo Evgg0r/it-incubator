8f3a3fc6b6a7ef9333914eb310031c66e6f7bc24
evgg0r <gor.evg2100@gmail.com>
2023-11-11 20:46:23 +0700

    commit

--- Sandbox/index4.js
+++ Sandbox/index4.js
@@ -20,7 +20,7 @@ let card1 = {
         },
         {
             title: 'Order Revenue',
-            amount: 874,
+            amount: -874,
             date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         }
     ]
@@ -37,7 +37,7 @@ let card2 = {
     isActive: false,
     transactions: [{
             title: 'Order Revenue',
-            amount: 874,
+            amount: -874,
             date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
         },
         {
@@ -76,12 +76,39 @@ document.write('<h1>', 'Hello ', +currentUser.name, '</h1>');
 document.write('<h2>', 'Cards; ', '</h2>');
 
 for (let i = 0; i < cards.length; i++) {
+    // render card 
     let card = cards[i];
-    document.write('<div>');
+    document.write('<div class="card-block">');
     document.write('<b>', card.type + ' card', '</b>', '<br>');
-    document.write('<b>', card.networkType, '</b>', '<br>');
+
+    let networkLogo = '';
+    switch (card.networkType) {
+        case 'visa':
+            networkLogo = '<img src = "./image/visa_logo.svg">';
+            break;
+        case 'mastercard':
+            networkLogo = '<img src = "./image/mastercard_logo.svg">';
+            break;
+    }
+
+    document.write('<b>', networkLogo, '</b>', '<br>');
     document.write('<b>', 'current balance', '</b>', '<br>');
-    document.write('<span>', card.currentBallance, '</span>', '<br>');
+
+    let currencySign = '';
+    switch (card.currencyType) {
+        case 'USD':
+            currencySign = '$';
+            break;
+        case 'EUR':
+            currencySign = '€';
+            break;
+        case 'GBP':
+            currencySign = '£';
+            break;
+
+    }
+
+    document.write('<span>', currencySign + card.currentBallance, '</span>', '<br>');
     document.write('<h4>', card.number, '<h4>');
     document.write('<span>',
         card.expirationMonth,
@@ -96,7 +123,25 @@ for (let i = 0; i < cards.length; i++) {
     for (let j = 0; j < card.transactions.length; j++) {
         let transaction = card.transactions[j];
         console.log(transaction);
-        document.write("<li>", transaction.title, ", ", transaction.date, ",", transaction.amount, "</li>");
+        document.write("<li>",
+            transaction.title, ", ",
+            transaction.date, ",",
+            "</li>");
+
+        // условный рендеринг
+        if (transaction.amount > 0) {
+            document.write(
+                '<span class = "income">',
+                transaction.amount,
+                "</span>"
+            );
+        } else {
+            document.write(
+                '<span class = "outcome">',
+                transaction.amount,
+                "</span>"
+            );
+        }
     }
 
     document.write("</ul>");
