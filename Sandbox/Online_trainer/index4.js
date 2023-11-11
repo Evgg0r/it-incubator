55e422f1c87699546ad2e7ac304a6afbf9b84f0a
evgg0r <gor.evg2100@gmail.com>
2023-11-11 22:48:11 +0700

    commit

---
+++ Sandbox/Online_trainer/index4.js
@@ -0,0 +1,105 @@
+let dayTraining1 = [{
+    dayOfWeek: 'Monday',
+    exercises: [{
+        nameExercise: 'Deadlift',
+        trainingApproach: [{
+            number: 'First',
+            weight: 50,
+            repetitions: 10,
+        }, {
+            number: 'Second',
+            weight: 40,
+            repetitions: 15,
+        }, {
+            number: 'Third',
+            weight: 35,
+            repetitions: 12,
+        }, {
+            number: 'Fourth',
+            weight: 35,
+            repetitions: 12,
+        }],
+        imageTechOfExecution: [
+            './image/Deadlift/image1.svg',
+            './image/Deadlift/image2.svg',
+            './image/Deadlift/image3.svg',
+        ],
+    }, {
+        nameExercise: 'Pull-ups & Chin-ups',
+        trainingApproach: [{
+            number: 'First',
+            weight: null,
+            repetitions: 10,
+        }, {
+            number: 'Second',
+            weight: null,
+            repetitions: 15,
+        }, {
+            number: 'Third',
+            weight: null,
+            repetitions: 12,
+        }, {
+            number: 'Fourth',
+            weight: null,
+            repetitions: 12,
+        }],
+        imageTechOfExecution: [
+            './image/Pull-ups&Chin-ups/image1.svg',
+            './image/Pull-ups&Chin-ups/image2.svg',
+            './image/Pull-ups&Chin-ups/image3.svg',
+        ],
+    }]
+}];
+
+let dayTraining2 = [{
+    dayOfWeek: 'Tuesday',
+    exercises: [{
+        nameExercise: 'Barbell squats',
+        trainingApproach: [{
+            number: 'First',
+            weight: 50,
+            repetitions: 10,
+        }, {
+            number: 'Second',
+            weight: 40,
+            repetitions: 15,
+        }, {
+            number: 'Third',
+            weight: 35,
+            repetitions: 12,
+        }, {
+            number: 'Fourth',
+            weight: 35,
+            repetitions: 12,
+        }],
+        imageTechOfExecution: [
+            './image/Barbell_squats/image1.svg',
+            './image/Barbell_squats/image2.svg',
+            './image/Barbell_squats/image3.svg',
+        ],
+    }, {
+        nameExercise: 'Dumbbell lunges',
+        trainingApproach: [{
+            number: 'First',
+            weight: 8,
+            repetitions: 10,
+        }, {
+            number: 'Second',
+            weight: 8,
+            repetitions: 15,
+        }, {
+            number: 'Third',
+            weight: 7,
+            repetitions: 12,
+        }, {
+            number: 'Fourth',
+            weight: 5,
+            repetitions: 12,
+        }],
+        imageTechOfExecution: [
+            './image/Pull-ups&Chin-ups/image1.svg',
+            './image/Pull-ups&Chin-ups/image2.svg',
+            './image/Pull-ups&Chin-ups/image3.svg',
+        ],
+    }]
+}];
