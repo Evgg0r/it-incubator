0962eedc846ddeb18578acd338ffff0424234d5c
evgg0r <gor.evg2100@gmail.com>
2023-11-15 19:41:33 +0700

    commit

--- Sandbox/Banking/index4.js
+++ Sandbox/Banking/index4.js
@@ -4,12 +4,97 @@ const currentUser = getCurrentUser();
 const cards = getCards();
 
 // render
-document.write('<h1>', 'Hello ', +currentUser.name, '</h1>');
-document.write('<h2>', 'Cards; ', '</h2>');
+renderHeader(currentUser.name)
 
 for (let i = 0; i < cards.length; i++) {
-    // render card 
-    let card = cards[i];
+    const card = cards[i];
+    renderCard(card);
+}
+
+function getCurrentUser() {
+    return {
+        name: "Alex",
+    }
+}
+
+function getCards() {
+    let card1 = {
+        type: 'debit',
+        networkType: 'mastercard',
+        currencyType: 'USD',
+        currentBallance: 5750.53,
+        number: 5282345678901289,
+        expirationYear: 2025,
+        expirationMonth: 9,
+        isActive: true,
+        transactions: [{
+                title: 'Order Revenue',
+                amount: 874,
+                date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+            },
+            {
+                title: 'Order Revenue',
+                amount: -874,
+                date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+            }
+        ]
+    };
+
+    let card2 = {
+        type: 'credit',
+        networkType: 'visa',
+        currencyType: 'EUR',
+        currentBallance: 1343.53,
+        number: 59232654321012032,
+        expirationYear: 2026,
+        expirationMonth: 4,
+        isActive: false,
+        transactions: [{
+                title: 'Order Revenue',
+                amount: -874,
+                date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+            },
+            {
+                title: 'Order Revenue',
+                amount: 874,
+                date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+            }
+        ]
+    };
+
+    let card3 = {
+        type: 'debit',
+        networkType: 'mastercard',
+        currencyType: 'GBP',
+        currentBallance: 3263.65,
+        number: 5543259349582930,
+        expirationYear: 2028,
+        expirationMonth: 3,
+        isActive: true,
+        transactions: [{
+                title: 'Order Revenue',
+                amount: 874,
+                date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+            },
+            {
+                title: 'Order Revenue',
+                amount: 874,
+                date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
+            }
+        ]
+    };
+
+    let cards = [card1, card2, card3];
+
+    return cards;
+}
+
+function renderHeader(userName) {
+    document.write('<h1>', 'Hello ', +userName, '</h1>');
+    document.write('<h2>', 'Cards; ', '</h2>');
+};
+
+function renderCard(card) {
     document.write('<div class="card-block">');
     document.write('<b>', card.type + ' card', '</b>', '<br>');
 
@@ -77,85 +162,8 @@ for (let i = 0; i < cards.length; i++) {
     }
 
     document.write("</ul>");
-    document.write('</div>');
+    document.write("</div>");
 
-    ;
-}
+    document.write("<hr>");
 
-function getCurrentUser() {
-    return {
-        name: "Alex",
-    }
-}
-
-function getCards() {
-    let card1 = {
-    type: 'debit',
-    networkType: 'mastercard',
-    currencyType: 'USD',
-    currentBallance: 5750.53,
-    number: 5282345678901289,
-    expirationYear: 2025,
-    expirationMonth: 9,
-    isActive: true,
-    transactions: [{
-            title: 'Order Revenue',
-            amount: 874,
-            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        },
-        {
-            title: 'Order Revenue',
-            amount: -874,
-            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        }
-    ]
-};
-
-let card2 = {
-    type: 'credit',
-    networkType: 'visa',
-    currencyType: 'EUR',
-    currentBallance: 1343.53,
-    number: 59232654321012032,
-    expirationYear: 2026,
-    expirationMonth: 4,
-    isActive: false,
-    transactions: [{
-            title: 'Order Revenue',
-            amount: -874,
-            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        },
-        {
-            title: 'Order Revenue',
-            amount: 874,
-            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        }
-    ]
-};
-
-let card3 = {
-    type: 'debit',
-    networkType: 'mastercard',
-    currencyType: 'GBP',
-    currentBallance: 3263.65,
-    number: 5543259349582930,
-    expirationYear: 2028,
-    expirationMonth: 3,
-    isActive: true,
-    transactions: [{
-            title: 'Order Revenue',
-            amount: 874,
-            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        },
-        {
-            title: 'Order Revenue',
-            amount: 874,
-            date: new Date(2022, 3, 25, 16, 43, 22) // Месяцы в JavaScript начинаются с 0, поэтому 3 - это апрель.
-        }
-    ]
-};
-
-let cards = [card1, card2, card3];
-
-return cards;
 }
