92baa9204039e97c0b4491e0ec3b69e896412ff5
evgg0r <gor.evg2100@gmail.com>
2023-11-20 19:42:20 +0700

    commit

--- codeWars/index.js
+++ codeWars/index.js
@@ -168,3 +168,37 @@
 //     }
 //     return count;
 // };
+
+//---------------------------
+
+// Given a string s, write a method(function) that will
+// return true
+// if its a valid single integer or floating number or false
+// if its not.
+
+// Valid examples, should
+// return true:
+
+//     isDigit("3")
+// isDigit("  3  ")
+// isDigit("-3.23")
+// should
+// return false:
+
+//     isDigit("3-4")
+// isDigit("  3   5")
+// isDigit("3 5")
+// isDigit("zero")
+
+
+// function isDigit(s) {
+//     if (s.length === 0) {
+//         return false;
+//     }
+//     if (!/^[+-]?\d+(\.\d+)?$/.test(s)) {
+//         return false;
+//     }
+//     return !isNaN(parseFloat(s));
+// }
+
+//---------------------------
