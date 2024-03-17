032c0f1585318f41a9af0b6a3470a8862d235fb6
evgg0r <gor.evg2100@gmail.com>
2024-03-17 21:26:55 +0700

    commit

--- codeWars/index.js
+++ codeWars/index.js
@@ -852,3 +852,40 @@
 
 //     return components.length === 1 ? components[0] : components.join(", ").replace(/,([^,]*)$/, " and$1");
 // }
+
+// //---------------------------
+
+// Take the following IPv4 address: 128.32 .10 .1
+
+// This address has 4 octets where each octet is a single byte(or 8 bits).
+
+// 1 st octet 128 has the binary representation: 10000000
+// 2n d octet 32 has the binary representation: 00100000
+// 3 rd octet 10 has the binary representation: 00001010
+// 4 th octet 1 has the binary representation: 00000001
+// So 128.32 .10 .1 == 10000000.00100000 .00001010 .00000001
+
+// Because the above IP address has 32 bits, we can represent it as the unsigned 32 bit number: 2149583361
+
+// Complete the
+// function that takes an unsigned 32 bit number and returns a string representation of its IPv4 address.
+
+// Examples
+
+// 2149583361 == > "128.32.10.1"
+// 32 == > "0.0.0.32"
+// 0 == > "0.0.0.0"
+
+// function int32ToIp(int32) {
+//     const binaryString = int32.toString(2).padStart(32, '0');
+//     const octets = [];
+
+//     for (let i = 0; i < 32; i += 8) {
+//         const octet = parseInt(binaryString.substr(i, 8), 2);
+//         octets.push(octet);
+//     }
+
+//     return octets.join('.');
+// }
+
+// //---------------------------
