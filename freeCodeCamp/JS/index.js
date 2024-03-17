eabf3379bc00e838e763113ef292a65b0b2d7c41
evgg0r <gor.evg2100@gmail.com>
2024-03-17 21:55:52 +0700

    commit

--- freeCodeCamp/JS/index.js
+++ freeCodeCamp/JS/index.js
@@ -188,6 +188,8 @@ function sum(arr, n) {
     // Only change code above this line
 }
 
+// -------------------------
+
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -195,3 +197,7 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
