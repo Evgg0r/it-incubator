59802f8b5a3b1ada2252c54d3792d2ff3ab1ce2d
evgg0r <gor.evg2100@gmail.com>
2024-03-24 19:54:48 +0700

    commit

--- codeWars/index.js
+++ codeWars/index.js
@@ -1602,3 +1602,20 @@
 // }
 
 // //---------------------------
+
+// Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so,
+//     for example:
+
+//     3-- > 15(3 * 5¹)
+// 10-- > 250(10 * 5²)
+// 200-- > 25000(200 * 5³)
+// 0-- > 0(0 * 5¹) -
+//     3-- > -15(-3 * 5¹)
+
+// function multiply(number) {
+//     const digits = Math.abs(number).toString().length; // Получаем количество цифр в числе
+//     const power = Math.pow(5, digits); // Возведение 5 в степень количества цифр
+//     return number * power; // Умножение числа на 5, возведенное в степень количества цифр
+// }
+
+// //---------------------------
