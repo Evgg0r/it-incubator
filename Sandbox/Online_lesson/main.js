b6837a12b482a671a047803edc2ef0114ea93272
evgg0r <gor.evg2100@gmail.com>
2024-07-29 00:47:46 +0700

    Completed homework 3 assignment for Lesson 2

--- Sandbox/Online_lesson/main.js
+++ Sandbox/Online_lesson/main.js
@@ -45,26 +45,25 @@
 //     }
 // }
 
-/*
-* Напишите код, который симулирует бросок двух шестигранных игральных кубиков и определяет, выпал ли выигрышный дубль.
-* В переменные dice1 и dice2 должны присваиваться случайные целые числа от 1 до 6.
-* Дубль считается выигрышным, если на обоих дайсах выпало одно и то же число, и это число больше 3.
-* Полученное булево значение сохраните в переменной isWinningDouble
-
-* Для успешного прохождения тестов не меняйте названия переменных!
-*/
-
-let dice1 = Math.floor(Math.random() * 7) + 1; // your code
-let dice2 = Math.floor(Math.random() * 7) + 1; // your code
-let isWinningDouble
-
-if (dice1 === dice2 && dice2 >= 3){
-  isWinningDouble = true;
+const score = 50 // тестовое значение, можно изменять
+let grade
+// your code
+switch (true) {
+    case (score >= 0 && score <= 49):
+        grade = "F";
+        break;
+    case (score >= 50 && score <= 69):
+        grade = "D";
+        break;
+    case (score >= 70 && score <= 79):
+        grade = "C";
+        break;
+    case (score >= 80 && score <= 89):
+        grade = "B";
+        break;
+    case (score >= 90 && score <= 100):
+        grade = "A"
+        break;
 }
-else {
-  isWinningDouble = false;
-}// your code
-console.log(isWinningDouble ? 'Выигрышный дубль!': 'Не выигрышный дубль.')
 
-console.log('Первый бросок: ' + dice1)
-console.log('Второй бросок: ' + dice2)
+console.log(grade);
